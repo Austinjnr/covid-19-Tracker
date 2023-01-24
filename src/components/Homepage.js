caf563bf3fc615e142bd616b583498021d21bdab
vivianne <viviannjoroge91@gmail.com>
2023-01-24 10:51:46 +0300

    update homepage.js

---
+++ src/components/Homepage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Homepage(){
+    return(
+        <div>
+            <h1>COVID-19 Tracker</h1>
+            <h3>WELCOME!</h3>
+            <p>COVID-19, also known as the coronavirus disease, 
+                is a highly contagious respiratory illness caused by
+                 the SARS-CoV-2 virus. It was first identified in Wuhan,
+                  China in 2019 and has since spread to affect people in 
+                  countries around the world</p>
+
+            
+        </div>
+
+    )
+}
+
+
+
+
+
+
+export default Homepage;
