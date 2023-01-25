2f41ac042264534197ecc69b28d12b593267b44c
Daniel <muchiridaniel59@gmail.com>
2023-01-24 23:04:47 -0500

    complete

--- src/components/SearchFunctionality.js
+++ src/components/SearchFunctionality.js
@@ -1,3 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Table from "./Table"
 
-const [search]
+function Search({ data }) {
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    const filteredData = data.filter(item => 
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            />
+          </div>
+        );
+      }
+      
+      export default Search;
