b33484509245f13ddcdff8ad9ad23415fb919d1b
austinjnr <austinobonyo@gmail.com>
2023-01-27 08:44:22 +0300

    imported the dependancies to metrics

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { Bar } from 'react-chartjs-2';
+import {Chart as ChartJS} from 'chart.js/auto'
 
-function BarGraph(){
-    return
+function BarGraph({Countries}){
+
+    
+    return<Bar data={Countries} />
 }
 
 export default BarGraph;
